82be1fa172e81f40b0f111eec12d51154129f8e7
rajeshyarlagadda-lab <ynv_rajesh@yahoo.com>
2022-06-15 10:56:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-j2mfdk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-j2mfdk)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { FailedComponent } from './failed/failed.component';
+
+const routes: Routes = [
+  {
+    path: 'landing/home/signup',
+    component: HomeComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [MsalGuard]
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login-failed',
+    component: FailedComponent
+  }
+];
+
+const isIframe = window !== window.parent && !window.opener; // Remove this line to use Angular Universal
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    // Don't perform initial navigation in iframes
+    initialNavigation: !isIframe ? 'enabled' : 'disabled' // Remove this line to use Angular Universal
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
